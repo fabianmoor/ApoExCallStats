677a8ca0a8e3ca9524d218edff635406e52f12ad
Fabian Moor Pucar <fabbem@Fabians-MBP.home>
2023-12-28 13:37:59 +0100

    Epic

--- backend/main.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, request, jsonify
-
-app = Flask(__name__)
-
-@app.route("/get-user/<user-id>")
-def get_user(user_id):
-
-if __name__ == '__main__':
-    app.run(debug=True)
